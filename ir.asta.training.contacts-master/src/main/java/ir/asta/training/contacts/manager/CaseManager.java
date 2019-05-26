7de1509c2da09bd700e0e8fafcda562fc9a4aef5
remassudi <50790308+remassudi@users.noreply.github.com>
2019-05-26 21:08:17 +0430

    Update CaseManager.java

--- ir.asta.training.contacts-master/src/main/java/ir/asta/training/contacts/manager/CaseManager.java
+++ ir.asta.training.contacts-master/src/main/java/ir/asta/training/contacts/manager/CaseManager.java
@@ -1,35 +1,22 @@
 package ir.asta.training.contacts.manager;
 
-import ir.asta.training.contacts.dao.AuthDAO;
 import ir.asta.training.contacts.dao.CaseDAO;
 import ir.asta.training.contacts.entities.CaseEntity;
-import ir.asta.training.contacts.entities.UserEntity;
 import ir.asta.wise.core.datamanagement.ActionResult;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Random;
 
-@Named("authNamed")
+@Named("caseNamed")
 public class CaseManager {
     @Inject
     private CaseDAO dao;
 
     @Transactional
-    public ActionResult<UserEntity> CreateNewCase(String title, String request,  String requestContent, String receiverContent) {
-        ActionResult<UserEntity> result = new ActionResult<>();
-        if (!dao.containsUser(username)) {
-            result.setData(dao.register(username, hashPassword(password), generateToken()));
-            result.setSuccess(true);
-        }
-        else {
-            result.setMessage("کاربر وجود دارد");
-        }
+    public ActionResult<CaseEntity> CreateNewCase(String title, String request,  String requestContent, String receiverContent) {
+        ActionResult<CaseEntity> result = new ActionResult<>();
+        result.setData(dao.createNewCase(title, request, requestContent, receiverContent));
+        result.setSuccess(true);
         return result;
     }
 
