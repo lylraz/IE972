b01f1c04f9838d8417ae2042b2d0d3660805c405
remassudi <50790308+remassudi@users.noreply.github.com>
2019-05-26 21:09:01 +0430

    Update CaseServiceImpl.java

--- ir.asta.training.contacts-master/src/main/java/ir/asta/training/contacts/services/impl/CaseServiceImpl.java
+++ ir.asta.training.contacts-master/src/main/java/ir/asta/training/contacts/services/impl/CaseServiceImpl.java
@@ -1,25 +1,19 @@
 package ir.asta.training.contacts.services.impl;
 
-import ir.asta.training.contacts.entities.UserEntity;
-import ir.asta.training.contacts.manager.AuthManager;
+import ir.asta.training.contacts.entities.CaseEntity;
 import ir.asta.training.contacts.manager.CaseManager;
-import ir.asta.training.contacts.services.AuthService;
 import ir.asta.training.contacts.services.CaseService;
 import ir.asta.wise.core.datamanagement.ActionResult;
-
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.ws.rs.FormParam;
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 
-@Named("authService")
+@Named("caseService")
 public class CaseServiceImpl implements CaseService {
     @Inject
     private CaseManager manager;
 
     @Override
-    public ActionResult<UserEntity> newCase(String title, String request,  String requestContent, String receiverContent) {
-        return
+    public ActionResult<CaseEntity> CreateNewCase(String title, String request, String requestContent, String receiverContent) {
+        return manager.CreateNewCase(title, request, requestContent, receiverContent);
     }
 }
