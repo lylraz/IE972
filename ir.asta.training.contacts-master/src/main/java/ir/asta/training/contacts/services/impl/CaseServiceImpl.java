3a8d739f7ee3d59cfa3d442b94a27a5c6e2bafca
remassudi <50790308+remassudi@users.noreply.github.com>
2019-06-22 22:07:41 +0430

    Update CaseServiceImpl.java

--- ir.asta.training.contacts-master/src/main/java/ir/asta/training/contacts/services/impl/CaseServiceImpl.java
+++ ir.asta.training.contacts-master/src/main/java/ir/asta/training/contacts/services/impl/CaseServiceImpl.java
@@ -16,4 +16,11 @@ public class CaseServiceImpl implements CaseService {
     public ActionResult<CaseEntity> CreateNewCase(String title, String request, String requestContent, String receiverContent) {
         return manager.CreateNewCase(title, request, requestContent, receiverContent);
     }
+    
+    public ActionResult<CaseEntity> Action(String title, String answer, String referContent, String statusContent) {
+        ActionResult<CaseEntity> result = new ActionResult<>();
+        result.setData(dao.action(title, answer, referContent, statusContent));
+        result.setSuccess(true);
+        return result;
+    }
 }
