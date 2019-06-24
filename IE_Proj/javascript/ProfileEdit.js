c0a6f9a3e937982adf78fddafbe498b46d6c53ba
remassudi <50790308+remassudi@users.noreply.github.com>
2019-06-24 20:53:46 +0430

    Update ProfileEdit.js

--- IE_Proj/javascript/ProfileEdit.js
+++ IE_Proj/javascript/ProfileEdit.js
@@ -16,3 +16,59 @@ function openGroupTab(evt, tabName) {
 function backB() {
     window.history.back();
 }
+
+function profileEdit() {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:8080/contacts/rest/auth/profileEdit",
+        "method": "POST",
+        "headers": {
+            "cache-control": "no-cache",
+            "postman-token": "5cccda77-1300-6257-8852-92079f7103c9"
+        },
+        "data": {
+            "email": localStorage.getItem("email"),
+            "name": document.getElementById("name").value,
+            "familyName": document.getElementById("familyName").value,
+            "profileName": document.getElementById("profileName").value,
+            "newEmail": document.getElementById("newEmail").value
+        }
+    }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        alert(response.message);
+        localStorage.setItem("email", document.getElementById("newEmail").value);
+    });
+
+    $.ajax(settings).fail(function (response) {
+        alert("ajax_fail");
+    });
+}
+
+function passChange() {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:8080/contacts/rest/auth/profileEdit",
+        "method": "POST",
+        "headers": {
+            "cache-control": "no-cache",
+            "postman-token": "5cccda77-1300-6257-8852-92079f7103c9"
+        },
+        "data": {
+            "email": localStorage.getItem("email"),
+            "password": document.getElementById("password").value,
+            "newPass": document.getElementById("newPass").value,
+            "reNewPass": document.getElementById("reNewPass").value
+        }
+    }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        alert(response.message);
+    });
+
+    $.ajax(settings).fail(function (response) {
+        alert("ajax_fail");
+    });
+}
