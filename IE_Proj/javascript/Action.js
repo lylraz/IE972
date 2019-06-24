cc6f04c3b4f7e0bdf80b4a6e4feaf292d0e56241
remassudi <50790308+remassudi@users.noreply.github.com>
2019-06-24 20:24:38 +0430

    Update Action.js

--- IE_Proj/javascript/Action.js
+++ IE_Proj/javascript/Action.js
@@ -1,24 +1,54 @@
 $(document).ready(function () {
-    $.getJSON("data.json",function (jdata) {
-        document.getElementById("title").innerText = jdata.requests[1].title;
-        document.getElementById("sender").innerText = jdata.requests[1].sender;
-        document.getElementById("request").innerText = jdata.requests[1].text;
-    })
+    document.getElementById("title").innerText = localStorage.getItem("title");
+    document.getElementById("sender").innerText = localStorage.getItem("sender");
+    document.getElementById("request").innerText = localStorage.getItem("request");
 });
 
-function referF() {
-    document.getElementById("referD").classList.toggle("show");
-}
+function action() {
 
-window.onclick = function(event) {
-    if (!event.target.matches('.referButton')) {
-        var dropdowns = document.getElementsByClassName("referContent");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:8080/contacts/rest/case/action",
+        "method": "POST",
+        "headers": {
+            "cache-control": "no-cache",
+            "postman-token": "5cccda77-1300-6257-8852-92079f7103c9"
+        },
+        "data": {
+            "title": localStorage.getItem("title"),
+            "answer": document.getElementById("answer").value,
+            "referContent": document.getElementById("referSelect").value,
+            "statusContent": document.getElementById("statusSelect").value
         }
     }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        alert(response.message);
+        if(response.success) {
+            window.location.href = "Cases.html";
+        }
+    });
+
+    $.ajax(settings).fail(function (response) {
+        alert("ajax_fail");
+        window.location.href ="Action.html";
+    });
 }
+
+// function referF() {
+//     document.getElementById("referD").classList.toggle("show");
+// }
+
+// window.onclick = function(event) {
+//     if (!event.target.matches('.referButton')) {
+//         var dropdowns = document.getElementsByClassName("referContent");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// };
