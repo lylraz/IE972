2691c5b450a5f59731463a42c3a4cc25de91c9e7
lylraz <50790436+lylraz@users.noreply.github.com>
2019-06-25 00:14:46 +0430

    Update SignInSignUp.js

--- IE_Proj/javascript/SignInSignUp.js
+++ IE_Proj/javascript/SignInSignUp.js
@@ -24,18 +24,53 @@ function register(){
     $.ajax(settings).done(function (response) {
       console.log(response);
       if(!response.success){
-        alert("no");
-        window.location.href ="SignIn.html";
+        alert(response.message);
         }
       else{
         alert("yes");
-        localStorage.setItem("fName",response.data.name);
-        localStorage.setItem("lName",response.data.name);
+        localStorage.setItem("fName",response.data.fName);
+        localStorage.setItem("lName",response.data.lName);
         localStorage.setItem("email",response.data.email);
         window.location.href = "Cases.html";
       }
     });
     $.ajax(settings).fail(function (response){
-          window.location.href ="Action.html";
+          alert("Fail");
         });
+}
+
+function login(){
+      localStorage.setItem("fName", "0");
+      localStorage.setItem("lName", "0");
+      localStorage.setItem("email", "0");
+
+      var settings = {
+      "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:8080/contacts/rest/auth/login",
+        "method": "POST",
+        "headers": {
+          "cache-control": "no-cache",
+          "postman-token": "1413b85e-c4c9-75c5-6fbf-c2aabb0e5808"
+        },
+      "data": {
+        "email": document.getElementById("email").value,
+        "password": document.getElementById("password").value,
+      }
     }
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      if(!response.success){
+        alert(response.message);
+        }
+      else{
+        localStorage.setItem("fName",response.data.fName);
+        localStorage.setItem("lName",response.data.lName);
+        localStorage.setItem("email",response.data.email);
+        window.location.href = "Cases.html";
+      }
+    });
+    $.ajax(settings).fail(function (response){
+    alert("Fail")
+        });
+}
