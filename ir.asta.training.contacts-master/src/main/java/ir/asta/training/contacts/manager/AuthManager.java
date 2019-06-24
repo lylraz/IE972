55a091ff6311e11c94f414178e45374a5eca2f85
lylraz <50790436+lylraz@users.noreply.github.com>
2019-06-24 22:17:38 +0430

    Update AuthManager.java

--- ir.asta.training.contacts-master/src/main/java/ir/asta/training/contacts/manager/AuthManager.java
+++ ir.asta.training.contacts-master/src/main/java/ir/asta/training/contacts/manager/AuthManager.java
@@ -12,6 +12,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
+import java.util.List;
 
 @Named("authNamed")
 public class AuthManager {
