b30a6055b32bc2d786907867d5569b0989e4a872
remassudi <50790308+remassudi@users.noreply.github.com>
2019-06-24 21:03:04 +0430

    Update Report.js

--- IE_Proj/javascript/Report.js
+++ IE_Proj/javascript/Report.js
@@ -1,54 +1,124 @@
-
 $(document).ready(function () {
-    $.getJSON("data.json",function (jdata) {
-        var adminPost = 0, memberPost = 0, criticism = 0, suggestion= 0, complaint = 0, request = 0, open = 0, inQueue = 0, postponed = 0, closed = 0, satisfied = 0, notSatisfied = 0;
-        for(var i = 0; i < jdata.requests.length; i++) {
-            if (jdata.requests[i].role !== "کاربر عادی") {
-                adminPost++;
-            } else {
-                memberPost++;
-            }
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:8080/contacts/rest/case/report",
+        "method": "POST",
+        "headers": {
+            "cache-control": "no-cache",
+            "postman-token": "5cccda77-1300-6257-8852-92079f7103c9"
+        },
+        "data": {}
+    }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        alert(response.message);
+        if (response.success) {
+            var adminPost = 0, memberPost = 0, criticism = 0, suggestion= 0, complaint = 0, request = 0, open = 0, inQueue = 0, postponed = 0, closed = 0, satisfied = 0, notSatisfied = 0;
+            for(var i = 0; i < response.length; i++) {
+                if (response[i].role !== "کاربر عادی") {
+                    adminPost++;
+                } else {
+                    memberPost++;
+                }
 
-            if (jdata.requests[i].kind === "انتقاد") {
-                criticism++;
-            } else if (jdata.requests[i].kind === "پیشنهاد") {
-                suggestion++;
-            } else if (jdata.requests[i].kind === "شکایت") {
-                complaint++;
-            } else {
-                request++;
-            }
+                if (response[i].kind === "انتقاد") {
+                    criticism++;
+                } else if (response[i].kind === "پیشنهاد") {
+                    suggestion++;
+                } else if (response[i].kind === "شکایت") {
+                    complaint++;
+                } else {
+                    request++;
+                }
 
-            if (jdata.requests[i].status === "باز") {
-                open++;
-            } else if (jdata.requests[i].status === "در حال بررسی") {
-                inQueue++;
-            } else if (jdata.requests[i].status === "تعویق") {
-                postponed++;
-            } else {
-                closed++;
-            }
+                if (response[i].status === "باز") {
+                    open++;
+                } else if (response[i].status === "در حال بررسی") {
+                    inQueue++;
+                } else if (response[i].status === "تعویق") {
+                    postponed++;
+                } else {
+                    closed++;
+                }
 
-            if (jdata.requests[i].status === "راضی") {
-                satisfied++;
-            } else  if (jdata.requests[i].status === "ناراضی"){
-                notSatisfied++;
+                if (response[i].status === "راضی") {
+                    satisfied++;
+                } else  if (response[i].status === "ناراضی"){
+                    notSatisfied++;
+                }
             }
+            document.getElementById("adminPost").innerText = adminPost.toString()+" مورد ";
+            document.getElementById("memberPost").innerText = memberPost.toString()+" مورد ";
+            document.getElementById("criticism").innerText = criticism.toString()+" مورد ";
+            document.getElementById("suggestion").innerText = suggestion.toString()+" مورد ";
+            document.getElementById("complaint").innerText = complaint.toString()+" مورد ";
+            document.getElementById("request").innerText = request.toString()+" مورد ";
+            document.getElementById("open").innerText = open.toString()+" مورد ";
+            document.getElementById("inQueue").innerText = inQueue.toString()+" مورد ";
+            document.getElementById("postponed").innerText = postponed.toString()+" مورد ";
+            document.getElementById("closed").innerText = closed.toString()+" مورد ";
+            document.getElementById("satisfied").innerText = satisfied.toString()+" مورد ";
+            document.getElementById("notSatisfied").innerText = notSatisfied.toString()+" مورد ";
         }
-        document.getElementById("adminPost").innerText = adminPost.toString()+" مورد ";
-        document.getElementById("memberPost").innerText = memberPost.toString()+" مورد ";
-        document.getElementById("criticism").innerText = criticism.toString()+" مورد ";
-        document.getElementById("suggestion").innerText = suggestion.toString()+" مورد ";
-        document.getElementById("complaint").innerText = complaint.toString()+" مورد ";
-        document.getElementById("request").innerText = request.toString()+" مورد ";
-        document.getElementById("open").innerText = open.toString()+" مورد ";
-        document.getElementById("inQueue").innerText = inQueue.toString()+" مورد ";
-        document.getElementById("postponed").innerText = postponed.toString()+" مورد ";
-        document.getElementById("closed").innerText = closed.toString()+" مورد ";
-        document.getElementById("satisfied").innerText = satisfied.toString()+" مورد ";
-        document.getElementById("notSatisfied").innerText = notSatisfied.toString()+" مورد ";
-    })
+    });
+    
+    $.ajax(settings).fail(function (response) {
+        alert("ajax_fail");
+    });
 });
+
+
+// $(document).ready(function () {
+//     $.getJSON("data.json",function (jdata) {
+//         var adminPost = 0, memberPost = 0, criticism = 0, suggestion= 0, complaint = 0, request = 0, open = 0, inQueue = 0, postponed = 0, closed = 0, satisfied = 0, notSatisfied = 0;
+//         for(var i = 0; i < jdata.requests.length; i++) {
+//             if (jdata.requests[i].role !== "کاربر عادی") {
+//                 adminPost++;
+//             } else {
+//                 memberPost++;
+//             }
+//
+//             if (jdata.requests[i].kind === "انتقاد") {
+//                 criticism++;
+//             } else if (jdata.requests[i].kind === "پیشنهاد") {
+//                 suggestion++;
+//             } else if (jdata.requests[i].kind === "شکایت") {
+//                 complaint++;
+//             } else {
+//                 request++;
+//             }
+//
+//             if (jdata.requests[i].status === "باز") {
+//                 open++;
+//             } else if (jdata.requests[i].status === "در حال بررسی") {
+//                 inQueue++;
+//             } else if (jdata.requests[i].status === "تعویق") {
+//                 postponed++;
+//             } else {
+//                 closed++;
+//             }
+//
+//             if (jdata.requests[i].status === "راضی") {
+//                 satisfied++;
+//             } else  if (jdata.requests[i].status === "ناراضی"){
+//                 notSatisfied++;
+//             }
+//         }
+//         document.getElementById("adminPost").innerText = adminPost.toString()+" مورد ";
+//         document.getElementById("memberPost").innerText = memberPost.toString()+" مورد ";
+//         document.getElementById("criticism").innerText = criticism.toString()+" مورد ";
+//         document.getElementById("suggestion").innerText = suggestion.toString()+" مورد ";
+//         document.getElementById("complaint").innerText = complaint.toString()+" مورد ";
+//         document.getElementById("request").innerText = request.toString()+" مورد ";
+//         document.getElementById("open").innerText = open.toString()+" مورد ";
+//         document.getElementById("inQueue").innerText = inQueue.toString()+" مورد ";
+//         document.getElementById("postponed").innerText = postponed.toString()+" مورد ";
+//         document.getElementById("closed").innerText = closed.toString()+" مورد ";
+//         document.getElementById("satisfied").innerText = satisfied.toString()+" مورد ";
+//         document.getElementById("notSatisfied").innerText = notSatisfied.toString()+" مورد ";
+//     })
+// });
 
 function filterSelection(c) {
     var x, i;
