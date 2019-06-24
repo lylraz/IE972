2ad9c7f704c04bf3d445bab12ce3c27f77414505
remassudi <50790308+remassudi@users.noreply.github.com>
2019-06-24 20:38:30 +0430

    Update CreateNewCase.js

--- IE_Proj/javascript/CreateNewCase.js
+++ IE_Proj/javascript/CreateNewCase.js
@@ -1,3 +1,4 @@
+
 function close_nav(){
     nav = document.getElementById("nav_bar");
     nav.style.transform ='translateX(100%)' ;
@@ -18,4 +19,32 @@ function Open_nav(){
     divv.style.marginRight = '32%';
     cButton = document.getElementById("confirmButton");
     cButton.style.marginRight = '82%';
+}
+
+function createNewCase() {
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:8080/contacts/rest/case/createNewCase",
+        "method": "POST",
+        "headers": {
+            "cache-control": "no-cache",
+            "postman-token": "5cccda77-1300-6257-8852-92079f7103c9"
+        },
+        "data": {
+            "title": document.getElementById("title").value,
+            "request": document.getElementById("requestText").value,
+            "requestContent": document.getElementById("requestSelect").value,
+            "receiverContent": document.getElementById("receiverSelect").value
+        }
+    }
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        alert(response.message);
+    });
+
+    $.ajax(settings).fail(function (response) {
+        alert("ajax_fail");
+    });
 }
